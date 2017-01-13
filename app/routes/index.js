35a4fc62cc977cde7d43d7e573b650aa464cf0e9
Kevin Finley <kftwotwo@gmail.com>
2017-01-13 08:45:56 -0800

    add firebase finally darn github

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  author: "Bob Joe",
+  question: "How do you work a computer?",
+  notes: "Computers are sutpid"
+}, {
+  id: 1,
+  author: "James Nick",
+  question: "What is JavaScript?",
+  notes: "JavaScript?"
+}, {
+  id: 1,
+  author: "Cooper Crabtree",
+  question: "Is a computer nerd cool?",
+  notes: "Nerds?"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
